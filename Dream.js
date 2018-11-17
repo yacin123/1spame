4963d02ab21f6a23aabe1971662f50abf7e10adf
yacin123 <44523421+yacin123@users.noreply.github.com>
2018-11-17 10:32:15 +0100

    Add files via upload

---
+++ Dream.js
@@ -0,0 +1,14 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("510860891496120323")
+setInterval(function() {
+channel.send(`youtube`);
+}, 25)
+})
+ 
+ 
+client.login('NTEwMzkzNTA4NDE1MTQzOTQ2.DsihDA.7RljtLo3A0SqEhrLQlDL_zfR5ec');
